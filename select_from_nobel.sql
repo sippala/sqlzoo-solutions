28172c4ddf311a2472e3fa00e144a42a0c4676b4
Shankar Ippala <ishan753@gmail.com>
2017-05-31 19:51:46 -0400

    Create select_from_nobel.sql

---
+++ select_from_nobel.sql
@@ -0,0 +1,39 @@
+/*
+Third section of sqlzoo, SELECT from Nobel
+*/
+
+--#1 
+# Change the query shown so that it displays Nobel prizes for 1950
+
+SELECT yr, subject, winner
+FROM nobel
+WHERE yr = 1950
+
+--#2 
+# Show who won the 1962 prize for Literature
+
+SELECT winner
+FROM nobel
+WHERE yr = 1962
+AND subject = 'Literature'
+
+--#3
+#Show the year and subject that won 'Albert Einstein' his prize
+
+SELECT yr, subject 
+FROM nobel 
+WHERE winner = 'Albert Einstein'
+
+--#4 
+# Give the name of the 'Peace' winners since the year 2000, including 2000
+
+SELECT winner 
+FROM nobel 
+WHERE subject = 'Peace' AND  yr >= 2000 
+
+--#5 
+# Show all details (yr, subject, winner) of the Literature prize winners for 1980 to 1989 inclusive.
+
+SELECT yr, subject, winner 
+FROM nobel 
+WHERE subject = 'Literature' AND yr >= 1980 and yr <= 1989 
